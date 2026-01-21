5d63a6ae7b950722ce95b0470bef6cf580859a78
Brandon Chavez <2203066293@cua.uam.mx>
2026-01-20 21:44:25 -0600

    Add complete project files

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/golpe'>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
